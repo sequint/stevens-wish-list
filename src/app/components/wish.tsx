55ba6963b81e5be3fb89226f38e6abedb9fca134
stevenquintana <steve.e.quint@gmail.com>
2023-11-27 13:22:49 -0800

    Add card links

--- src/app/components/wish.tsx
+++ src/app/components/wish.tsx
@@ -1,13 +1,18 @@
 import Image from 'next/image'
 import { WishProps } from '../interfaces/WishProps'
 import styles from './wish.module.css'
+import Link from 'next/link'
 
 export default function Wish(props: WishProps) {
   const { title, url, image } = props
 
   return (
     <>
-      <div className={ styles.wishCardContainer }>
+      <Link
+        className={ styles.wishCardContainer }
+        href={url}
+        target='_blank'
+      >
         <Image
         className={ styles.wishCardImg }
           src={ image }
@@ -16,7 +21,7 @@ export default function Wish(props: WishProps) {
           alt='Wish Item'
         />
         <p className={ styles.wishCardText }>{ title }</p>
-      </div>
+      </Link>
     </>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ import Wish from './components/wish'
 import styles from './page.module.css'
 
 const wishes = [
-  { id: 1, title: 'Book 1', url: 'sample', image: zToOneImg },
-  { id: 2, title: 'Book 2', url: 'sample again', image: engAtGoogImg }
+  { id: 1, title: 'Book 1', url: 'https://www.amazon.com/s?k=zero+to+one&i=stripbooks&crid=14SKG86LRJTO7&sprefix=zero+to+one%2Cstripbooks%2C145&ref=nb_sb_ss_ts-doa-p_1_11', image: zToOneImg },
+  { id: 2, title: 'Book 2', url: 'https://www.amazon.com/Software-Engineering-Google-Lessons-Programming/dp/1492082791/ref=rvi_sccl_4/144-5130831-2377427?pd_rd_w=BCyO2&content-id=amzn1.sym.f5690a4d-f2bb-45d9-9d1b-736fee412437&pf_rd_p=f5690a4d-f2bb-45d9-9d1b-736fee412437&pf_rd_r=BYY3SAPM2YFXWE0780YM&pd_rd_wg=nOB6l&pd_rd_r=ae7e4198-5811-4bcb-81d5-3c541405cd0c&pd_rd_i=1492082791&psc=1', image: engAtGoogImg }
 ]
 
 export default function Home() {
