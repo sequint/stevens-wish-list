c7c115c52bfb42060b19406eeec6b86d564461ae
stevenquintana <steve.e.quint@gmail.com>
2023-11-27 16:01:50 -0800

    Create function to alternate title colors and make site responsive

--- src/app/components/title.tsx
+++ src/app/components/title.tsx
@@ -1,9 +1,35 @@
+import { Fragment } from 'react'
 import styles from './title.module.css'
 
 export default function Title() {
+  const titleArr: string[] = [
+    'S', 't', 'e', 'v', 'e', 'n', '\'', 's', ' ',
+    'W', 'i', 's', 'h', ' ',
+    'L', 'i', 's', 't'
+  ]
+  let currentCharColor: string = 'green'
+
+  function getCurrentColor(): string {
+    const returnColor: string = currentCharColor
+
+    currentCharColor = currentCharColor === 'green' ? 'red' : 'green'
+
+    return returnColor;
+  }
+
+  function getTitle() {
+    let charId: number = 1
+
+    return titleArr.map(char =>
+      <Fragment key={charId}>
+        <span style={{ color: getCurrentColor()}}>{ char }</span>
+      </Fragment>
+    )
+  }
+
   return (
     <>
-      <h1 className={styles.pageTitle}>Stevens Wish List</h1>
+      <h1 className={styles.pageTitle}>{getTitle()}</h1>
     </>
   )
 }
